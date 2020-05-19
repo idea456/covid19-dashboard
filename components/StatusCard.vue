2c3e6b6b4e31357e6d1eacc1f34add1dc2d0aba2
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-05-19 19:40:23 +0800

    fixed export error

--- components/StatusCard.vue
+++ components/StatusCard.vue
@@ -2,21 +2,31 @@
   <v-card color="white" dark width="250" height="150" max-width="400">
     <v-card-text>
       <p class="display-1 text--primary status-number">
-        <strong><ICountUp :delay="1000" :endVal="number"/></strong>
+        <strong>
+          <AnimatedNumber
+            :value="number"
+            :duration="1500"
+            :formatValue="formatNumber"
+          />
+        </strong>
       </p>
       <div class="text--primary status-text">{{ title }}</div>
     </v-card-text>
   </v-card>
 </template>
 
-<script>
-import ICountUp from "vue-countup-v2";
-
+<script type="text/babel">
+import AnimatedNumber from "animated-number-vue";
 export default {
   name: "StatusCard",
   props: [`title`, `number`, `color`],
   components: {
-    ICountUp
+    AnimatedNumber
+  },
+  methods: {
+    formatNumber(number) {
+      return Math.floor(number);
+    }
   }
 };
 </script>
