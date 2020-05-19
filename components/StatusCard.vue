244201ba47d6edd58f4c988963eb536652fadc88
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-05-19 12:54:09 +0800

    changed color of status cards

--- components/StatusCard.vue
+++ components/StatusCard.vue
@@ -1,12 +1,10 @@
 <template>
-  <v-card :color="color" dark width="250" height="150" max-width="400">
+  <v-card color="white" dark width="250" height="150" max-width="400">
     <v-card-text>
       <p class="display-1 text--primary status-number">
         <strong>{{ number }}</strong>
       </p>
-      <div class="text--primary status-text">
-        {{ title }}
-      </div>
+      <div class="text--primary status-text">{{ title }}</div>
     </v-card-text>
   </v-card>
 </template>
